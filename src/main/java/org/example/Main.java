207c900089f0f3a90e7b222adf9711c152ee20ed
mr.detafti <dehghani735@gmail.com>
2024-02-01 09:26:53 +0330

    Change from declarative way to programming way.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,23 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import org.example.entities.Book;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        String puName = "my-persistence-unit";
+        Map<String, String> props = new HashMap<>();
+
+        EntityManagerFactory emf =
+//                Persistence.createEntityManagerFactory("my-persistence-unit");
+                new HibernatePersistenceProvider()
+                        .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
         EntityManager em = emf.createEntityManager();
 
         try {
